020919431df9b2dc7e1afbec5800a21fed391dd0
Remo Schwarzentruber <Remo.Schwarzentruber@stud.hslu.ch>
2019-09-27 20:00:38 +0200

    fix script create semesterweek

--- create_new_semesterweek.sh
+++ create_new_semesterweek.sh
@@ -22,7 +22,42 @@ cp ${TEMPLATE_MAKEFILE} ${FOLDERNAME}/
 cp ${TEMPLATE_METAFILE} ${FOLDERNAME}/
 
 # Replace content of Makefile
-sed -i "" "s/01_einfuehrung/${SEMESTERWEEK}_${TOPIC_LOWERCASE}/" ${FOLDERNAME}/${MAKEFILE}
+# sed -i "" "s/01_einfuehrung/${SEMESTERWEEK}_${TOPIC_LOWERCASE}/" ${FOLDERNAME}/${MAKEFILE}
+python -c "
+import sys
+
+param_replace = str(sys.argv[1])
+param_file = str(sys.argv[2])
+
+# Read in the file
+with open(param_file, 'r') as file :
+    filedata = file.read()
+
+# Replace the target string
+filedata = filedata.replace('01_einfuehrung', param_replace)
+print(type(filedata))
+
+# Write the file out again
+with open(param_file, 'w') as file :
+    file.write(filedata)
+" ${SEMESTERWEEK}_${TOPIC_LOWERCASE} ${FOLDERNAME}/${MAKEFILE}
 
 # Replace content of 00-meta.md
-sed -i "" "s/SW01/SW${SEMESTERWEEK}/" ${FOLDERNAME}/${METAFILE}
+# sed -i "" "s/SW01/SW${SEMESTERWEEK}/" ${FOLDERNAME}/${METAFILE}
+python -c "
+import sys
+
+param_replace = str(sys.argv[1])
+param_file = str(sys.argv[2])
+
+# Read in the file
+with open(param_file, 'r') as file :
+    filedata = file.read()
+
+# Replace the target string
+filedata = filedata.replace('SW01', param_replace)
+
+# Write the file out again
+with open(param_file, 'w') as file :
+    file.write(filedata)
+" SW${SEMESTERWEEK} ${FOLDERNAME}/${METAFILE}
