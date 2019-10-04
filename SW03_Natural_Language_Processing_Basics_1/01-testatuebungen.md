0fe09e50fc4c383a70e647433dfcc34d83a5dbcc
Remo Schwarzentruber <Remo.Schwarzentruber@stud.hslu.ch>
2019-10-04 14:36:49 +0200

    start with nlp

--- SW03_Natural_Language_Processing_Basics_1/01-testatuebungen.md
+++ SW03_Natural_Language_Processing_Basics_1/01-testatuebungen.md
@@ -1,1 +1,60 @@
 # Testatübung SW03
+
+## Aufgabe 0 - Installations Hinweis
+
+```bash
+export NLTK_DATA=~/Documents/Studium.Local/KBDS/nltk_data
+```
+
+Packages:
+```python
+import nltk
+nltk.download('punkt')
+nltk.download('averaged_perceptron_tagger')
+nltk.download('maxent_ne_chunker')
+nltk.download('words')
+nltk.download('stopwords')
+nltk.download('wordnet')
+```
+
+## Aufgabe 1 - Tokenizieren
+
+Tokenizieren sie folgenden Text:
+```
+When Alexander Graham Bell invented the telephone he had three missed calls from Chuck Norris.
+```
+
+### Screenshot
+
+
+### Code
+
+```python
+tbd
+```
+
+## Aufgabe 2 - Stop Words
+
+Entfernen sie die Stop Words aus dem tokenizierten Text.
+
+### Screenshot
+
+
+### Code
+
+```python
+tbd
+```
+
+## Aufgabe 3 - Stemming / Lemmatization
+
+Führen sie auf dem gleichen Text Stemming und Lemmatization aus. (Welche Probelmatik entdecken sie in den Resultaten?)
+
+### Screenshot
+
+
+### Code
+
+```python
+tbd
+```
---
+++ SW03_Natural_Language_Processing_Basics_1/NLP.py
@@ -0,0 +1,97 @@
+"""
+Knowledge-based Decision Support Systems
+    Natural Language Processing Basics 1
+"""
+
+from nltk import tokenize
+from nltk import tag 
+from nltk import chunk 
+from nltk.stem import PorterStemmer, WordNetLemmatizer
+from nltk.tokenize import sent_tokenize, word_tokenize
+from nltk.corpus import stopwords
+from nltk import pos_tag
+from nltk.tokenize import word_tokenize
+
+
+NLP_TEXT = "I am a student at the Hochschule Luzern. I love IT."
+
+def main():
+    print("")
+    print("Knowledge-based Decision Support Systems")
+    print("\tNatural Language Processing Basics 1")
+    print("")
+    print("Satz Tokenizierung")
+    satz_tokenizierung()
+    print("Wort Tokenizierung")
+    word_tokenizierung()
+    print("Part of Speech Tagging")
+    part_of_speech_tagging()
+    print("NE Chunking")
+    ne_chucking()
+    print("Wort Stemming")
+    wort_stemming()
+    print("Satz Stemming")
+    satz_stemming()
+    print("Stop Word Removal")
+    stop_word_removal()
+    print("Wort Stemming & Lemmatization")
+    wort_stemming_and_lemmatization()
+
+
+def satz_tokenizierung():
+    satz = tokenize.sent_tokenize(NLP_TEXT)
+    print(satz)
+
+def word_tokenizierung():
+    token_wort = tokenize.word_tokenize(NLP_TEXT) 
+    print(token_wort)
+    return token_wort
+
+def part_of_speech_tagging():
+    tagged_pos = "I am a student at the Hochschule Luzern. I love IT."  
+    tagged_pos = tag.pos_tag(word_tokenizierung()) 
+    print(tagged_pos)
+    return tagged_pos
+
+def ne_chucking():
+    tree = chunk.ne_chunk(part_of_speech_tagging())
+    # print(tree)
+    # tree.draw()
+
+def wort_stemming():
+    ps = PorterStemmer()
+    example_words = ["go","goes","nogo","begone","gone"]
+    for w in example_words:
+        print(ps.stem(w))
+
+def satz_stemming():
+    ps = PorterStemmer()
+    words = word_tokenize(NLP_TEXT)
+    for w in words:
+        print(ps.stem(w))
+
+def stop_word_removal():
+    stop_words = set(stopwords.words('english'))
+    word_tokens = word_tokenize(NLP_TEXT)
+    #filtered_sentence = [w for w in word_tokens if not w in stop_words]
+    filtered_sentence = []
+
+    for w in word_tokens:
+        if w not in stop_words:
+            filtered_sentence.append(w)
+
+    print(word_tokens)
+    print(filtered_sentence)
+
+
+def wort_stemming_and_lemmatization():
+    stemmer = PorterStemmer()
+    lemmatiser = WordNetLemmatizer()
+
+    wort = "going"
+
+    print("Stem %s: %s" % ((wort), stemmer.stem((wort))))
+    print("Lemmatise %s: %s" % ((wort), lemmatiser.lemmatize((wort))))
+
+if __name__ == "__main__":
+    main()
